e07926496571e0235823bb76bf6e97b8e77cd46c
YounesHouhou <133326039+YounesHouhou@users.noreply.github.com>
2024-07-04 10:20:04 +0200

    Add files via upload

--- Franka_gym_pos.py
+++ Franka_gym_pos.py
@@ -7,12 +7,14 @@ from manipulation.scenarios import AddShape, SetTransparency
 from manipulation.utils import ConfigureParser,FindDataResource, RenderDiagram, running_as_notebook
 
 
+#Register the env returned by make_Franka_env() under the name of "Franka-v1"
+
 gym.envs.register(
     id="Franka-v1",
     entry_point=("Franka_gym_pos:make_Franka_env"),
 )
 
-def AddFranka(plant):
+def AddFranka(plant): 
 
     parser = Parser(plant)
     ConfigureParser(parser)
@@ -43,7 +45,10 @@ class RewardSystem(LeafSystem):
         actions = self.get_input_port(1).Eval(context)
         output[0] = 0 
 
-class TransformSystem(LeafSystem):
+#LeafSystem that uses the chosen action and the current end effector (EE) position 
+#as inputs to compute a new target cartesian position for the end effector, performing a translation based on the chosen action.
+
+class TransformSystem(LeafSystem): 
     def __init__(self):
         LeafSystem.__init__(self)
         
@@ -73,16 +78,15 @@ class TransformSystem(LeafSystem):
             new_pos = np.array([0.,0.,-0.001]) 
        
         franka_pose = RigidTransform(p= new_pos + X_AE.translation(), R= X_AE.rotation())
-        #PoseEE = RigidTransform(R=RotationMatrix(R=np.array([[0, 0, 1], [0, -1, 0], [1, 0, 0]])), p=np.array([0, 0, 0.0585])) 
-        #franka_pose = franka_pose.multiply(PoseEE.inverse())
+   
        
         output.set_value(franka_pose)
 
-def make_franka_move(meshcat=None):
+# Build Drake diagram and simulator
+def make_franka_move(meshcat=None): 
     builder = DiagramBuilder()
     plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.001)
     plant.set_discrete_contact_approximation(DiscreteContactApproximation.kSap)
-    # TODO(russt): randomize parameters.
     panda_model,peg_model,hole_model = AddFranka(plant)
     plant.Finalize()
     plant.set_name("plant")
@@ -106,28 +110,16 @@ def make_franka_move(meshcat=None):
     ki = [1] * plant.num_positions()
     kd = [10] * plant.num_positions()
 
-    #
-    #builder.ExportInput(franka_ik_solver.GetInputPort("X_AE_desired"))
-    #builder.ExportInput(franka_ik_solver.GetInputPort("use_robot_state"))
-    #builder.ExportOutput(franka_ik_solver.get_output_port())
-    builder.Connect(plant.get_state_output_port(panda_model), franka_ik_solver.GetInputPort("robot_state"))
 
-    #
+    builder.Connect(plant.get_state_output_port(panda_model), franka_ik_solver.GetInputPort("robot_state"))
 
     franka_controller = builder.AddSystem(InverseDynamicsController(plant, kp, ki, kd, False))
     franka_controller.set_name("franka_controller")
 
     actions = builder.AddSystem(PassThrough(1))
-    #positions_to_state = builder.AddSystem(Multiplexer([plant.num_positions(), plant.num_positions()]))
-    #zeros = builder.AddSystem(ConstantVectorSource([0] * plant.num_positions()))
 
-    #builder.Connect(actions.get_output_port(), positions_to_state.get_input_port(0))
-    
-    #builder.Connect(zeros.get_output_port(), positions_to_state.get_input_port(1))
-    #builder.Connect(positions_to_state.get_output_port(),franka_controller.get_input_port_desired_state())
     builder.Connect(franka_controller.get_output_port_control(), plant.get_actuation_input_port())
     builder.Connect(plant.get_state_output_port(panda_model), franka_controller.get_input_port_estimated_state())
-
     builder.ExportOutput(plant.get_contact_results_output_port())
     builder.ExportInput(actions.get_input_port(), "actions")
     builder.ExportOutput(plant.get_state_output_port(), "observations")
@@ -139,7 +131,6 @@ def make_franka_move(meshcat=None):
     builder.Connect(plant.get_state_output_port(panda_model), reward.get_input_port(0))
     builder.Connect(actions.get_output_port(), reward.get_input_port(1))
     builder.Connect(actions.get_output_port(), transform.get_input_port(0))
-
     builder.Connect(transform.get_output_port(),franka_ik_solver.get_input_port(0)) #
 
     positions_to_state = builder.AddSystem(Multiplexer([plant.num_positions(), plant.num_positions()]))
@@ -162,39 +153,19 @@ def make_franka_move(meshcat=None):
     franka_ik_solver_context = franka_ik_solver.GetMyMutableContextFromRoot(context)
 
     q0 = np.array([-1.57, 0.1, 0, -1.2, 0, 1.6, 0])
-    #plant.SetPositions(plant_context, q0)
-    plant.SetDefaultPositions(q0)
-
+    
+    plant.SetDefaultPositions(q0) #Fix default pos to handle potential issue with franka ik solver
     diagram.ForcedPublish(context)
 
-    x0 = np.hstack((q0, 0*q0)) # Initial State Variable
-    plant.SetPositions(plant_context, q0)
-    
-    p0=np.array([0.51610254 ,0.00644409, 0.15879793])
-    r0= Quaternion(np.array([7.06856869e-01, -3.00474510e-04 , 7.07353135e-01, -2.19491010e-03]))
-    r0=RotationMatrix(r0)
-    franka_init_pose = RigidTransform(p=p0, R=r0)
-    
-    #franka_ik_solver.GetInputPort("use_robot_state").FixValue(franka_ik_solver_context,True)
-    #franka_controller.GetInputPort("desired_state").FixValue(controller_context, x0)
-
-    #franka_joint_desired = franka_ik_solver.GetOutputPort("joint_positions").Eval(franka_ik_solver_context)
-    #franka_joint_desired = np.hstack([franka_joint_desired, np.zeros(7)])
-    #franka_controller.GetInputPort("desired_state").FixValue(controller_context, franka_joint_desired)
-
-    
     return simulator
 
-def make_Franka_env(meshcat=None):
+#Define action/obs space and create a gym env using DrakeGymEnv()
+def make_Franka_env(meshcat=None): 
 
     simulator = make_franka_move(meshcat=meshcat)
-    #action_space = gym.spaces.Discrete(3)
-    action_space = gym.spaces.Box(low=0, high= 5, shape=(1,), dtype=np.int32) 
+    #action_space = gym.spaces.Discrete(3) Not working with drake
+    action_space = gym.spaces.Box(low=0, high= 5, shape=(1,), dtype=np.int32) # Set a discrete action space with 6 possible actions
     
-    #clipped_action_space = np.clip(action_space, action_space.low, action_space.high)
-    
-    
-
     plant = simulator.get_system().GetSubsystemByName("plant")
 
     low = np.concatenate(
@@ -221,7 +192,7 @@ def make_Franka_env(meshcat=None):
         simulator.Initialize()
         
         plant_context = plant.GetMyMutableContextFromRoot(context)
-        initial_positions = np.array([-1.57, 0.1, 0, -1.2, 0, 2.6, 0])
+        initial_positions = np.array([-1.57, 0.1, 0, -1.2, 0, 1.6, 0])
         initial_velocities = np.zeros_like(initial_positions)
         
         plant.SetPositions(plant_context, initial_positions)
@@ -240,12 +211,12 @@ def make_Franka_env(meshcat=None):
     
     return env
 
-def main():
+#def main():
 
-    env= make_Franka_env()  
+    #env= make_Franka_env()  
 
  #   simulator=make_franka_move()
     #RenderDiagram(simulator.get_system(), max_depth=1)
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
